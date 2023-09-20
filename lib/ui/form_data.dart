1ebf6d19a344819a026ce484d94cbc08ad3e516e
Yunic10 <yudithnico99@gmail.com>
2023-09-20 13:30:16 +0700

    Udah Ya

--- lib/ui/form_data.dart
+++ lib/ui/form_data.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tugas_2/ui/tampil_data.dart';
 
 class FormData extends StatefulWidget {
-  const FormData({Key? key}) : super(key: key);
+  const FormData({super.key});
 
   @override
-  _FormDataState createState() => _FormDataState();
+  State<FormData> createState() => _FormDataState();
 }
 
 class _FormDataState extends State<FormData> {
+  
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _nimController = TextEditingController();
+  final TextEditingController _tahunController = TextEditingController();
+
+  dynamic _dataField (TextEditingController controller, String label){
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(labelText: label),
+      ),
+    );
+  }
+  
+  void simpan(){
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => TampilData(
+            nama: _namaController.text,
+            nim: _nimController.text,
+            tahun: int.parse(_tahunController.text),
+      )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Input Data"),
+        title: const Text("Input Data"),
       ),
+      body : SafeArea(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            children: [
+              _dataField(_namaController, 'Nama'),
+              _dataField(_nimController, 'NIM'),
+              _dataField(_tahunController, 'Tahun Lahir'),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 140),
+                child: ElevatedButton(
+                onPressed: (){
+                  simpan();
+                  }, 
+                  child: const Text("Simpan"),
+                )
+                ),              
+            ],
+          ),
+        )
+        )
     );
   }
 }
